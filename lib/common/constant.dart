fb8e38f3ee58faf18822c33d69d644f3501f2a3c
Alvi M. Azirin <31821107+alviazirin@users.noreply.github.com>
2024-04-22 07:03:00 +0700

    Initial commit

---
+++ lib/common/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const colorPrimary = Color(0xFF357AD4);
+const colorSecondary = Color(0xFF27B1E0);
+const colorTitle = Color(0xFF14142B);
+const colorBody = Color(0xFF4E4B66);
+const colorLabel = Color(0xFF85868B);
