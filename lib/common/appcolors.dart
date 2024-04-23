b78277b4cbf5f68cf0bd075338db74594aaa55e3
Alvi M. Azirin <31821107+alviazirin@users.noreply.github.com>
2024-04-23 07:17:49 +0700

    feat: splash page

---
+++ lib/common/appcolors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF357AD4);
+  static const Color secondary = Color(0xFF27B1E0);
+  static const Color title = Color(0xFF14142B);
+  static const Color body = Color(0xFF4E4B66);
+  static const Color label = Color(0xFF85868B);
+  static const Color placeholder = Color(0xFFBFC0C2);
+  static const Color line = Color(0xFFE4E4E5);
+  static const Color inputBackground = Color(0xFFF6F6F6);
+  static const Color background = Color(0xFFF9F9F9);
+  static const Color offWhite = Color(0xFFFCFCFC);
+
+  static final ThemeData appThemeData = ThemeData(
+    primaryColor: primary,
+    secondaryHeaderColor: secondary,
+    primaryTextTheme: const TextTheme(
+      titleLarge: TextStyle(color: title),
+    ),
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(color: title),
+    ),
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: primary,
+      background: primary,
+    ),
+  );
+}
--- lib/common/presentation/splash.dart
+++ lib/common/presentation/splash.dart
@@ -7,9 +7,51 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Tween<double> animationSize;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    Future.microtask(() {});
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    animationSize = Tween(begin: 1, end: 2);
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Center(
+          child: TweenAnimationBuilder<double>(
+            duration: const Duration(seconds: 1),
+            tween: animationSize,
+            builder: (context, value, child) {
+              return Center(
+                child: Transform.scale(
+                  scale: value,
+                  child: Image.asset(
+                    "assets/images/edspert_logo.png",
+                    width: 160,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
